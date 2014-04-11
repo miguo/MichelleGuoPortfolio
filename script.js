31a7ffc46d646ea589b61ef317fc7a4549d27d7e
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2014-04-11 09:27:39 +0800

    v.1.13
     - Changed navigation menu to circles (using FullPage.js)
     - Added Email to contact page

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-			
+
 	var pepe = $.fn.fullpage({
 		slidesColor: ['whitesmoke', '#FF884D', '#FF884D', '#1A8094', '#41C4AB'],
 		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
 		menu: '#menu'
 	});
-		
+
 	/* Moves screenshots up and down on hover */
 	$('.hover').mouseover(function() {
   		$(this).addClass("hover_in");
@@ -15,9 +15,7 @@ $(document).ready(function() {
 	$('.hover').mouseout(function() {
   		$(this).addClass("hover_out");
   		$(this).removeClass("hover_in");
-	}); 
+	});
 
 
 });
-
-
